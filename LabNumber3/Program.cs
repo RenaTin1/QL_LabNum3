bd9b0b18c5439e1863ece6c20564d8e63f7a63ff
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-09-28 00:30:48 -0400

    QL Lab #3

---
+++ LabNumber3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LAB NUMBER 3- Serena Tindle
+
+            bool ContinueOn = true;
+            while (ContinueOn == true)
+            {
+
+                int x;
+                //User Input
+                Console.WriteLine("Learn your squares and cubes"); 
+                Console.WriteLine("Enter an integer:"); //ask for a integer
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number" + "\t" + "Square" + "\t" + "Cubed"); //name columns
+                for (int i= 0; i <= x; i++) //Process
+                {
+                    Console.WriteLine(i +"\t" + Math.Pow(i,2) + "\t" + Math.Pow(i, 3)); //print value of integer squared an cubed
+                }
+                string Userinput;
+                Console.WriteLine("Continue? (Y / N)");
+                Userinput = Console.ReadLine();
+                if (Userinput == "N" || Userinput =="No" || Userinput== "no") //User decides to continue or not
+                {
+                    ContinueOn = false;
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
